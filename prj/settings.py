f6488ac7e72dab17a7f80ec19d45a6116bba1ff9
Akshaj1234454 <akshaj17apr@gmail.com>
2025-07-20 02:27:45 +0530

    Fix CSRF and allowed hosts for Railway

--- prj/settings.py
+++ prj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['https://crud-basics-production-7dc1.up.railway.app/', '127.0.0.1', 'localhost']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
